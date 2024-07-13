fedf39619a48b49f90ea717fdc43e398dff42442
destinyobs <destinyobeuh14@gmail.com>
2024-07-13 18:23:45 +0100

    update

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+SMTP_HOST = "smtp-relay.sendinblue.com"
+SMTP_USER = "destinyobueh14@gmail.com"
+SMTP_PASSWORD = "wIzNjV9dZF5T6qsh"
+EMAILS_FROM_EMAIL = "info@example.com"
+SMTP_TLS = True
+SMTP_SSL = False
+SMTP_PORT = 587
